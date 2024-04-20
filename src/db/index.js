1e21dc6ee517a37a4b4ec01b1a45048b7a4b34dc
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-04-20 14:32:34 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstanse = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`\n MongoDB connected || DB HOST : ${connectionInstanse}`);
+  } catch (error) {
+    console.error("MongoDB connection error", error);
+    process.exit;
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
-import mongoose from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
+dotenv.config({
+  path: "./env",
+});
+connectDB();
+
+/*
+;(async()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+    } catch (error) {
+        console.error("error in connect db " , error)
+        throw error
+    }
+})()
+
+
+*/
