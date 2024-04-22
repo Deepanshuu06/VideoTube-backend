87e628e31921727cf5f30b6f3e000a21a74f65fa
deepanshu baghel <106313726+Deepanshuu06@users.noreply.github.com>
2024-04-22 16:11:36 +0530

    adding controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyjwt } from "../middlewares/auth.middleware.js";
@@ -26,5 +31,10 @@ router.route("/login").post(loginUser);
 
 router.route("/logout").post(verifyjwt, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/update-avatar").post(updateUserAvatar);
+router.route("/update-cover-image").post(updateUserCoverImage);
+router.route("/change-password").post(changeCurrentPassword);
+router.route("/update-account-details").post(updateAccountDetails);
+router.route("/get-current-user").post( getCurrentUser);
 
 export default router;
